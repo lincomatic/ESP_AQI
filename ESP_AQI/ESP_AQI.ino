6f525b4765462f9ad72c657e74d4411a83fe2a7e
scl <lincomatic@gmail.com>
2018-12-09 13:50:50 -0800

    Wemos D1 Pro, dual PMS support

--- ESP_AQI/ESP_AQI.ino
+++ ESP_AQI/ESP_AQI.ino
@@ -13,38 +13,35 @@
 // ON LOLIN DON'T USE GPIO5/4 -> I2C!!!
 //#define PIN_RX 5// LoLin/WeMos D1/GPIO5
 //#define PIN_TX 4 // LoLin/WeMos D2/GPIO4
-#define PIN_RX  14 // LoLin/WeMos D5/GPIO14
-#define PIN_TX  15 // LoLin/WeMos D8/GPIO15 - dummy-not hooked up
+//#define PIN_RX  14 // LoLin/WeMos D5/GPIO14
+#define PIN_RX1 13 // D7GPIO13 PMS5003
+#define PIN_RX2 12 // D5/GPIO12 PMS7003
+#define PIN_TX  15 // LoLin/WeMos D8/GPIO1pin_fac5 - dummy-not hooked up
 //#define PIN_TX  12 // LoLin/WeMos D6/GPIO12
-#define PIN_SET 13  // LoLin/WeMos D7/GPIO13
+//#define PIN_SET 13  // LoLin/WeMos D7/GPIO13
+#define PIN_SET 14 // D5/GPIO14
 //#define PIN_LED 2 //0
-#define PIN_FACTORY_RESET 12 // LoLin D6/GPIO12 ground this pin to wipe out EEPROM & WiFi settings
+//#define PIN_FACTORY_RESET 12 // LoLin D6/GPIO12 ground this pin to wipe out EEPROM & WiFi settings
 
 #define AP_PREFIX "ESPAQI_"
 #define TEMPERATURE_FAHRENHEIT
 //#define THINGSPEAK
 #define EMONCMS
 
-
-
-
-
-
 #define UPDATE_INTERVAL_MS 300000UL
+//#define UPDATE_INTERVAL_MS 35000UL
 // delay after wake up before taking a reading
-#define PMS_SLEEP_WAKEUP_WAIT 30000UL
+#define PMS_SLEEP_WAKEUP_WAIT 32000UL
 
 #ifdef THINGSPEAK
 #define API_WRITEKEY_LEN 16
-//#define THINGSPEAK_WRITE_KEY "thingspeak-write-key"
+#define THINGSPEAK_WRITE_KEY "thingspeak-write-key"
 #endif
 
 #ifdef EMONCMS
 #define EMONCMS_NODE "aqi0"
-//#define EMONCMS_WRITE_KEY "emoncms-write-key"
-//#define EMONCMS_BASE_URI "http://data.openevse.com/emoncms/input/post.json?node="
-#define EMONCMS
-#define EMONCMS_BASE_URI "http://www.lincomatic.com/emoncms/input/post.json?node="
+#define EMONCMS_WRITE_KEY "emoncms-write-key"
+#define EMONCMS_BASE_URI "http://data.openevse.com/emoncms/input/post.json?node="
 #define API_WRITEKEY_LEN 32
 #endif
 
@@ -58,12 +55,19 @@ ArduinoOTAMgr AOTAMgr;
 #endif
 
 
-Pmsx003 pms(PIN_RX, PIN_TX);
+Pmsx003 pms1(PIN_RX1, PIN_TX);
+#ifdef PIN_RX2
+Pmsx003 pms2(PIN_RX2, PIN_TX);
+#endif // PIN_RX2
 AM2320 am2320;
+
+#ifdef API_WRITEKEY_LEN
 char g_ApiWriteKey[API_WRITEKEY_LEN+1] = { 0 };
+#endif
 
 void reboot()
 {
+  Serial.println("rebooting...");
   WiFi.disconnect();
   delay(1000);
   ESP.reset();
@@ -76,7 +80,9 @@ void reboot()
 #include "./btn.h"
 
 
+#ifdef PIN_FACTORY_RESET
 Btn btnReset(PIN_FACTORY_RESET);
+#endif
 
 ////////////////////////////////////////
 
@@ -94,22 +100,28 @@ void factoryReset()
 // n.b. sleep command is flaky, so use SET pin instead for sleep/wake
 void pmsx003sleep()
 {
+#ifdef PIN_SET
   digitalWrite(PIN_SET,LOW);
+#endif
 }
 
 void pmsx003wake()
 {
+#ifdef PIN_SET
   digitalWrite(PIN_SET,HIGH);
+#endif
 }
 
 
 void backgroundTasks()
 {
+#ifdef PIN_FACTORY_RESET
   btnReset.read();
   if (btnReset.longPress()) {
     Serial.println("long press");
     factoryReset();
   }
+#endif
 
 #ifdef OTA_UPDATE
   AOTAMgr.handle();
@@ -130,11 +142,23 @@ void mydelay(unsigned long ms)
 
 void setup(void)
 {
-  pms.begin();
-  pms.waitForData(Pmsx003::wakeupTime);
-  pms.write(Pmsx003::cmdModeActive);
+  pms1.begin();
+  pms1.waitForData(Pmsx003::wakeupTime);
+  pms1.write(Pmsx003::cmdModeActive);
+#ifdef PIN_RX2
+  pms2.begin();
+  pms2.waitForData(Pmsx003::wakeupTime);
+  pms2.write(Pmsx003::cmdModeActive);
+#endif // PIN_RX2
+#ifdef PIN_SET
   pinMode(PIN_SET, OUTPUT); // sleep/wake pin
+ #ifndef PMS_SLEEP_WAKEUP_WAIT
+  pmsx003wake();
+ #endif
+#endif
+#ifdef PMS_SLEEP_WAKEUP_WAIT
   pmsx003sleep();
+#endif
 
   // onboard leds also outputs
 #ifdef PIN_LED
@@ -145,7 +169,9 @@ void setup(void)
   Serial.begin(115200);
   while (!Serial) {};
 
+#ifdef PIN_FACTORY_RESET
   btnReset.init();
+#endif
   
 #ifdef WIFI_MGR
   EEPROM.begin(g_wfCfg.getConfigSize());  
@@ -181,7 +207,48 @@ void setup(void)
 
 unsigned long lastUpdateMs = 0UL;
 unsigned long updateWaitMs = 0UL;
-char g_sTmp[256];
+char g_sTmp[512];
+
+const auto n = Pmsx003::Reserved;
+Pmsx003::pmsData data[n];
+#ifdef PIN_RX2
+Pmsx003::pmsData data2[n];
+#endif // PIN_RX2
+
+int readPms(Pmsx003 *pms,Pmsx003::pmsData *data)
+{
+  Serial.println("readPms");
+  pms->flushInput();
+
+  int retry = 0;
+  Pmsx003::PmsStatus status;
+  do {
+    Serial.println("_________________");
+    status = pms->read(data, n);
+    switch(status) {
+    case Pmsx003::OK:
+      {
+	for (size_t i = 0; i < n; ++i) { 
+	  Serial.print(data[i]);
+	  Serial.print("\t");
+	  Serial.print(Pmsx003::dataNames[i]);
+	  Serial.print(" [");
+	  Serial.print(Pmsx003::metrics[i]);
+	  Serial.print("]");
+	  Serial.println();
+	}
+      }
+      break;
+    default:
+      ++retry;
+      Serial.println(Pmsx003::errorMsg[status]);
+      /*if (status == Pmsx003::noData)*/ mydelay(500);
+    } // switch(status)
+
+  } while ((status != Pmsx003::OK) && (retry < 10));    
+
+  return (status == Pmsx003::OK) ? 0 : 1;
+}
 
 void loop(void)
 {
@@ -192,104 +259,90 @@ void loop(void)
 #ifdef PIN_LED
     digitalWrite(PIN_LED,LOW); // onboard LED on
 #endif // PIN_LED
-    pmsx003wake();
     unsigned long waitms = millis();
+
+#ifdef PMS_SLEEP_WAKEUP_WAIT
+    pmsx003wake();
     Serial.println("WAKEUP");
     mydelay(PMS_SLEEP_WAKEUP_WAIT);
-    pms.flushInput();
+#endif // PMS_SLEEP_WAKEUP_WAIT
+
     Serial.println("READ");
-    const auto n = Pmsx003::Reserved;
-    Pmsx003::pmsData data[n];
-
-    Pmsx003::PmsStatus status = pms.read(data, n);
-    do {
-      status = pms.read(data, n);
-  
-      switch (status) {
-      case Pmsx003::OK:
-	{
-	  Serial.println("_________________");
-	  Serial.print("Wait time ");
-	  waitms = millis() - waitms;
-	  Serial.println(waitms);
+    int rc1 = readPms(&pms1,data);
+    int rc2 = readPms(&pms2,data2);
 	  
-	  // For loop starts from 3
-	  // Skip the first three data (PM1dot0CF1, PM2dot5CF1, PM10CF1)
-	  for (size_t i = 0; i < n; ++i) { 
-	    Serial.print(data[i]);
-	    Serial.print("\t");
-	    Serial.print(Pmsx003::dataNames[i]);
-	    Serial.print(" [");
-	    Serial.print(Pmsx003::metrics[i]);
-	    Serial.print("]");
-	    Serial.println();
-	  }
-	  
-	  Serial.print("---\nam2320: ");
-	  bool arc = am2320.measure();
-	  float humidity,temperature;
-	  if (arc == true) {
-	    temperature =  am2320.getTemperature();
+    Serial.print("---\nam2320: ");
+    bool arc = am2320.measure();
+    float humidity,temperature;
+    if (arc == true) {
+      temperature =  am2320.getTemperature();
 #ifdef TEMPERATURE_FAHRENHEIT
-	    temperature = temperature * (9.0/5.0) + 32.0;
+      temperature = temperature * (9.0/5.0) + 32.0;
 #endif // TEMPERATURE_FAHRENHEIT
-	    humidity = am2320.getHumidity();
-	    sprintf(g_sTmp,"temp=%0.0f humidity=%0.0f",temperature,humidity);
-	  }
-	  else { // error
-	    //	    temperature = -99;
-	    //	    humidity = -1;
-	    sprintf(g_sTmp,"error %d",am2320.getErrorCode());
-	  }
-	  Serial.println(g_sTmp);
-	  
-	  if (*g_ApiWriteKey) {
-	    *g_sTmp = 0;
-
+      humidity = am2320.getHumidity();
+      sprintf(g_sTmp,"temp=%0.0f humidity=%0.0f",temperature,humidity);
+    }
+    else { // error
+      //	    temperature = -99;
+      //	    humidity = -1;
+      sprintf(g_sTmp,"error %d",am2320.getErrorCode());
+    }
+    Serial.println(g_sTmp);
+    
+#ifdef API_WRITEKEY_LEN
+    if (*g_ApiWriteKey) {
+      *g_sTmp = 0;
+      
 #ifdef THINGSPEAK
-	    sprintf(g_sTmp,"http://api.thingspeak.com/update?api_key=%s&field1=%d&field2=%d&field3=%d",g_ApiWriteKey,data[Pmsx003::PM1dot0],data[Pmsx003::PM2dot5],data[Pmsx003::PM10dot0]);
-	    if (1) { //(arc == true) {
-	      sprintf(g_sTmp+strlen(g_sTmp),"&field4=%0.0f&field5=%0.0f",temperature,humidity);
-	    }
-#elif defined(EMONCMS)
-	    const char *baseuri = EMONCMS_BASE_URI;
-	    const char *node = EMONCMS_NODE;
-	    sprintf(g_sTmp,"%s%s&apikey=%s&json={pm1:%d,pm25:%d,pm10:%d,pm1cf1:%d,pm25cf1:%d,pm10cf1:%d,ppd03:%d,ppd05:%d,ppd1:%d,ppd25:%d,ppd50:%d,ppd10:%d",baseuri,node,EMONCMS_WRITE_KEY,data[Pmsx003::PM1dot0],data[Pmsx003::PM2dot5],data[Pmsx003::PM10dot0],data[Pmsx003::PM1dot0CF1],data[Pmsx003::PM2dot5CF1],data[Pmsx003::PM10dot0CF1],data[Pmsx003::Particles0dot3],data[Pmsx003::Particles0dot5],data[Pmsx003::Particles1dot0],data[Pmsx003::Particles2dot5],data[Pmsx003::Particles5dot0],data[Pmsx003::Particles10]);
-	    if (arc == true) {
-	      sprintf(g_sTmp+strlen(g_sTmp),",temp:%0.0f,rh:%0.0f",temperature,humidity);
-	    }
-	    strcat(g_sTmp,"}");
-#endif // EMONCMS
-	    if (*g_sTmp) {
-	      Serial.println(g_sTmp);
-	      HTTPClient http;
-	      http.setUserAgent("ESP_AQI/1.0");
-	      http.begin(g_sTmp);
-	      int hrc = http.GET(); // send request
-	      String hresp = http.getString(); // get payload
-	      Serial.print("return code: ");Serial.println(hrc);
-	      Serial.print("response data: ");Serial.println(hresp);
-	      http.end();
-	    }
-	  }
-	  else {
-	    Serial.println("API write key not set");
-	  }
-	  curms = millis();
-          Serial.print("updatems: ");Serial.println(curms-lastUpdateMs);
-	  updateWaitMs = UPDATE_INTERVAL_MS - waitms;
-	  lastUpdateMs = curms;
-	  pmsx003sleep();
-	}
-	break;
-      default:
-	Serial.println("_________________");
-	Serial.println(Pmsx003::errorMsg[status]);
-        if (status == Pmsx003::noData) mydelay(500);
-	break;
+      sprintf(g_sTmp,"http://api.thingspeak.com/update?api_key=%s&field1=%d&field2=%d&field3=%d",g_ApiWriteKey,data[Pmsx003::PM1dot0],data[Pmsx003::PM2dot5],data[Pmsx003::PM10dot0]);
+      if (1) { //(arc == true) {
+	sprintf(g_sTmp+strlen(g_sTmp),"&field4=%0.0f&field5=%0.0f",temperature,humidity);
       }
-	  backgroundTasks();
-    } while(status != Pmsx003::OK);
+#elif defined(EMONCMS)
+      const char *baseuri = EMONCMS_BASE_URI;
+      const char *node = EMONCMS_NODE;
+      sprintf(g_sTmp,"%s%s&apikey=%s&json={",baseuri,node,EMONCMS_WRITE_KEY);
+      if (!rc1) {
+	sprintf(g_sTmp+strlen(g_sTmp),"pm1:%d,pm25:%d,pm10:%d,pm1cf1:%d,pm25cf1:%d,pm10cf1:%d,ppd03:%d,ppd05:%d,ppd1:%d,ppd25:%d,ppd50:%d,ppd10:%d",data[Pmsx003::PM1dot0],data[Pmsx003::PM2dot5],data[Pmsx003::PM10dot0],data[Pmsx003::PM1dot0CF1],data[Pmsx003::PM2dot5CF1],data[Pmsx003::PM10dot0CF1],data[Pmsx003::Particles0dot3],data[Pmsx003::Particles0dot5],data[Pmsx003::Particles1dot0],data[Pmsx003::Particles2dot5],data[Pmsx003::Particles5dot0],data[Pmsx003::Particles10]);
+      }
+      if (!rc2) {
+	if (!rc1) strcat(g_sTmp,",");
+	sprintf(g_sTmp+strlen(g_sTmp),"pm1_2:%d,pm25_2:%d,pm10_2:%d,pm1cf1_2:%d,pm25cf1_1:%d,pm10cf1_2:%d,ppd03_2:%d,ppd05_2:%d,ppd1_2:%d,ppd25_2:%d,ppd50_2:%d,ppd10_2:%d",data2[Pmsx003::PM1dot0],data2[Pmsx003::PM2dot5],data2[Pmsx003::PM10dot0],data2[Pmsx003::PM1dot0CF1],data2[Pmsx003::PM2dot5CF1],data2[Pmsx003::PM10dot0CF1],data2[Pmsx003::Particles0dot3],data2[Pmsx003::Particles0dot5],data2[Pmsx003::Particles1dot0],data2[Pmsx003::Particles2dot5],data2[Pmsx003::Particles5dot0],data2[Pmsx003::Particles10]);
+      }
+      if (arc == true) {
+	if (!rc1 || !rc2) strcat(g_sTmp,",");
+	sprintf(g_sTmp+strlen(g_sTmp),"temp:%0.0f,rh:%0.0f",temperature,humidity);
+      }
+
+      if (!rc1 || !rc2 || arc) strcat(g_sTmp,",");
+      sprintf(g_sTmp+strlen(g_sTmp),"rssi:%d}",WiFi.RSSI());
+
+#endif // EMONCMS
+      if (*g_sTmp) {
+	Serial.println(g_sTmp);
+	HTTPClient http;
+	http.setUserAgent("ESP_AQI/1.0");
+	http.begin(g_sTmp);
+	int hrc = http.GET(); // send request
+	String hresp = http.getString(); // get payload
+	Serial.print("return code: ");Serial.println(hrc);
+	Serial.print("response data: ");Serial.println(hresp);
+	http.end();
+      }
+    }
+    else {
+      Serial.println("API write key not set");
+    }
+#endif // API_WRITEKEY_LEN
+
+    curms = millis();
+    Serial.print("updatems: ");Serial.println(curms-lastUpdateMs);
+    updateWaitMs = UPDATE_INTERVAL_MS - waitms;
+    lastUpdateMs = curms;
+#ifdef PMS_SLEEP_WAKEUP_WAIT
+    pmsx003sleep();
+#endif // PMS_SLEEP_WAKEUP_WAIT
+
 #ifdef PIN_LED
     digitalWrite(PIN_LED,HIGH); // onboard LED off
 #endif // PIN_LED
